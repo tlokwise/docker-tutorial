e044b3270cd83f2eb777a5216730fef9813104ec
tlokwise <tlokwisedev@outlook.comM>
2024-01-22 16:04:47 +0200

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import LoginForm from "./ui/form/LoginForm";
+import Navbar from "./ui/navbar/Navbar";
+
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <LoginForm />
     </main>
   );
